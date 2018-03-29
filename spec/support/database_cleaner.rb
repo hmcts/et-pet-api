5a6ec4c2d12c43f90d2fd70f116a64ccd5cc9287
Gary Taylor <gary.taylor@hismessages.com>
2018-03-29 07:54:28 +0100

    Re configured database cleaner

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -4,10 +4,6 @@ DatabaseCleaner.strategy = :truncation, { except: ['offices'] }
 
 RSpec.configure do |c|
   c.before db_clean: :true do
-    DatabaseCleaner.start
-  end
-
-  c.after db_clean: :true do
     DatabaseCleaner.clean
   end
 end
