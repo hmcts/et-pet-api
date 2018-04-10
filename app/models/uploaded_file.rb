0a31bd3b9fe72a2b7e1701074604478a8a124a49
Gary Taylor <gary.taylor@hismessages.com>
2018-04-10 21:25:35 +0100

    minor fix to uploaded file model

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -2,7 +2,7 @@ class UploadedFile < ApplicationRecord
   has_one_attached :file
   include StoredFileDownload
   def file_attributes=(attrs)
-    file.attach(attrs[:file])
+    file.attach(attrs)
   end
 
 end
