dfdf45a0784a0b75b57622db275bcdb2bdec9fc4
Gary Taylor <gary.taylor@hismessages.com>
2018-04-11 06:55:56 +0100

    Refactored respondent spec to use the address factory instead of hard coded hashes

--- spec/models/respondent_spec.rb
+++ spec/models/respondent_spec.rb
@@ -4,40 +4,23 @@ require 'rails_helper'
 
 RSpec.describe Respondent, type: :model do
   subject(:respondent) { described_class.new }
+  let(:example_address_attrs) { attributes_for :address }
 
   it 'loads address from nested attributes' do
     # Act
-    respondent.address_attributes = {
-      building: '102',
-      street: 'Petty France',
-      locality: 'London',
-      county: 'Greater London',
-      post_code: 'SW1H 9AJ'
-    }
+    respondent.address_attributes = example_address_attrs
 
     # Assert
-    expect(respondent.address).to have_attributes building: '102',
-                                                  street: 'Petty France',
-                                                  locality: 'London',
-                                                  county: 'Greater London',
-                                                  post_code: 'SW1H 9AJ'
+    expect(respondent.address).to have_attributes example_address_attrs
   end
 
   it 'loads work_address from nested attributes' do
+    # Arrange
+    example_address_attrs = attributes_for :address
     # Act
-    respondent.work_address_attributes = {
-      building: '102',
-      street: 'Petty France',
-      locality: 'London',
-      county: 'Greater London',
-      post_code: 'SW1H 9AJ'
-    }
+    respondent.work_address_attributes = example_address_attrs
 
     # Assert
-    expect(respondent.work_address).to have_attributes building: '102',
-                                                       street: 'Petty France',
-                                                       locality: 'London',
-                                                       county: 'Greater London',
-                                                       post_code: 'SW1H 9AJ'
+    expect(respondent.work_address).to have_attributes example_address_attrs
   end
 end
