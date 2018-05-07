ee627acf0030ffdc6da0bc2dc691aa11d186861f
Gary Taylor <gary.taylor@hismessages.com>
2018-05-07 12:57:06 +0100

    Ready to start first ET3 integration point

---
+++ app/controllers/api/v2/respondents/responses_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Api
+  module V2
+    module Respondents
+      class ResponsesController < ::ApplicationController
+        def create
+          debug_me=true
+        end
+
+      end
+    end
+  end
+end
