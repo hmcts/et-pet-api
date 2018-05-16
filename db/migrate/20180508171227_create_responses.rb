d3254f522c3c025edd8c8e08769574d95ab5c05e
Gary Taylor <gary.taylor@hismessages.com>
2018-05-16 17:08:13 +0100

    Corrected a couple of migrations

--- db/migrate/20180508171227_create_responses.rb
+++ db/migrate/20180508171227_create_responses.rb
@@ -9,16 +9,16 @@ class CreateResponses < ActiveRecord::Migration[5.2]
       t.string :claimants_name
       t.boolean :agree_with_early_conciliation_details
       t.string :disagree_conciliation_reason
-      t.string :agree_with_employment_dates
+      t.boolean :agree_with_employment_dates
       t.date :employment_start
       t.date :employment_end
       t.string :disagree_employment
-      t.string :continued_employment, :boolean
+      t.boolean :continued_employment
       t.boolean :agree_with_claimants_description_of_job_or_title
       t.string :disagree_claimants_job_or_title
-      t.string :agree_with_claimants_hours, :boolean
+      t.boolean :agree_with_claimants_hours
       t.decimal :queried_hours, precision: 4, scale: 2
-      t.string :agree_with_earnings_details, :boolean
+      t.boolean :agree_with_earnings_details
       t.decimal :queried_pay_before_tax, precision: 8, scale: 2
       t.string :queried_pay_before_tax_period
       t.decimal :queried_take_home_pay, precision: 8, scale: 2
