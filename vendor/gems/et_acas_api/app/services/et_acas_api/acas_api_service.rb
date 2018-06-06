2bfbf5350879f654a7302302c8e683921400ee0e
Gary Taylor <gary.taylor@hismessages.com>
2018-06-06 06:53:16 +0100

    The signature has now been verified in the spec

--- vendor/gems/et_acas_api/app/services/et_acas_api/acas_api_service.rb
+++ vendor/gems/et_acas_api/app/services/et_acas_api/acas_api_service.rb
@@ -1,13 +1,15 @@
 require 'savon'
 require 'base64'
 require 'openssl'
+require 'et_acas_api/soap_signature'
 module EtAcasApi
   class AcasApiService
-    def initialize(wsdl_url:, current_time: Time.zone.now, acas_rsa_certificate_path:, rsa_certificate_path: )
+    def initialize(wsdl_url:, current_time: Time.zone.now, acas_rsa_certificate_path:, rsa_certificate_path:, rsa_private_key_path: )
       self.wsdl_url = wsdl_url
       self.current_time = current_time
       self.acas_rsa_certificate = OpenSSL::X509::Certificate.new File.read(acas_rsa_certificate_path)
       self.rsa_certificate = OpenSSL::X509::Certificate.new File.read(rsa_certificate_path)
+      self.rsa_private_key = OpenSSL::PKey::RSA.new(File.read(rsa_private_key_path))
     end
 
     def get_certificate(id, user_id:)
@@ -41,11 +43,11 @@ module EtAcasApi
       @client ||= Savon.client wsdl: wsdl_url,
                                wsse_timestamp: true,
                                wsse_signature:
-                                   Akami::WSSE::Signature.new(
-                                       Akami::WSSE::Certs.new(cert_string: rsa_certificate))
+                                   SoapSignature.new(
+                                       Akami::WSSE::Certs.new(cert_string: rsa_certificate, private_key_string: rsa_private_key))
 
     end
 
-    attr_accessor :wsdl_url, :current_time, :acas_rsa_certificate, :rsa_certificate
+    attr_accessor :wsdl_url, :current_time, :acas_rsa_certificate, :rsa_certificate, :rsa_private_key
   end
 end
--- vendor/gems/et_acas_api/spec/services/et_acas_api/acas_api_service_spec.rb
+++ vendor/gems/et_acas_api/spec/services/et_acas_api/acas_api_service_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 RSpec.describe EtAcasApi::AcasApiService do
   subject(:api) { described_class.new config }
   let(:our_certificate_path) { File.absolute_path(File.join('..', '..', 'acas_interface_support', 'x509', 'ours', 'publickey.cer'), __dir__) }
-  let(:our_base64_public_key) { Base64.encode64(OpenSSL::X509::Certificate.new(File.read(our_certificate_path)).to_der).tr("\n", '') }
+  let(:our_private_key_path) { File.absolute_path(File.join('..', '..', 'acas_interface_support', 'x509', 'ours', 'privatekey.pem'), __dir__) }
+  let(:our_certificate) { OpenSSL::X509::Certificate.new(File.read(our_certificate_path)) }
+  let(:our_base64_public_key) { Base64.encode64(our_certificate.to_der).tr("\n", '') }
   let(:config) { {
       wsdl_url: 'http://mydomain.com/my.wsdl',
       current_time: Time.zone.parse('31/12/2017 18:00:00'),
       acas_rsa_certificate_path: File.absolute_path(File.join('..', '..', 'acas_interface_support', 'x509', 'theirs', 'publickey.cer'), __dir__),
       rsa_certificate_path: our_certificate_path,
+      rsa_private_key_path: our_private_key_path
   } }
   let(:example_get_certificate_url) { "https://localhost/Lookup/ECService.svc" }
   let(:wsdl_content) { File.read(File.absolute_path(File.join('..', '..', 'acas_interface_support', 'wsdl.txt'), __dir__)) }
@@ -36,7 +39,7 @@ RSpec.describe EtAcasApi::AcasApiService do
       expect(get_certificate_stub.with(body: body_matcher)).to have_been_requested
     end
 
-    it 'requests the data with the correct security token in the header' do
+    it 'requests the data with the correct security token in the signature in the header' do
       get_certificate_stub = stub_request(:post, example_get_certificate_url).to_return body: '', status: 200, headers: { 'Content-Type' => 'application/xml' }
       subject.get_certificate('anyid', user_id: "my user id")
       body_matcher = hash_including('env:Envelope' =>
@@ -49,6 +52,51 @@ RSpec.describe EtAcasApi::AcasApiService do
                                                 )
                                         ))
       expect(get_certificate_stub.with(body: body_matcher)).to have_been_requested
+    end
+
+    it 'requests the data with the correct digest value in the signature in the header' do
+      # Arrange - Build a stub which will record the request for later testing
+      recorded_request = nil
+      get_certificate_stub = stub_request(:post, example_get_certificate_url).to_return do |r|
+        recorded_request = r
+        { body: '', status: 200, headers: { 'Content-Type' => 'application/xml' } }
+      end
+
+      # Act - Call the service
+      subject.get_certificate('anyid', user_id: "my user id")
+
+      # Assert - Stage 1 - Calculate the expected digest value which uses nokokiri to canonicalize it correctly
+      doc = Nokogiri::XML(recorded_request.body)
+      node = doc.xpath('//env:Envelope/env:Header/wsse:Security/wsu:Timestamp', doc.collect_namespaces).first
+      digest_value = Base64.encode64(OpenSSL::Digest::SHA1.digest(node.canonicalize(Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0))).strip
+
+      # Assert - Stage 2 - Test the digest value is correct
+      xml = Hash.from_xml(recorded_request.body)
+      expect(xml.dig('Envelope', 'Header', 'Security', 'Signature', 'SignedInfo', 'Reference', 'DigestValue')).to eql digest_value
+    end
+
+    it 'requests the data with the correct signature value in the header' do
+      # Arrange - Build a stub which will record the request for later testing
+      recorded_request = nil
+      get_certificate_stub = stub_request(:post, example_get_certificate_url).to_return do |r|
+        recorded_request = r
+        { body: '', status: 200, headers: { 'Content-Type' => 'application/xml' } }
+      end
+
+      # Act - Call the service
+      subject.get_certificate('anyid', user_id: "my user id")
+
+      # Assert - Stage 1 - Calculate the expected signature value which uses nokokiri to canonicalize it correctly
+      doc = Nokogiri::XML(recorded_request.body)
+      ns = doc.collect_namespaces
+      ns['xmlns:ds'] = ns.delete('xmlns')
+      signed_info_node = doc.xpath('//env:Envelope/env:Header/wsse:Security/ds:Signature/ds:SignedInfo', ns).first
+      signature_value_node = doc.xpath('//env:Envelope/env:Header/wsse:Security/ds:Signature/ds:SignatureValue', ns).first
+      signature_value = Base64.decode64(signature_value_node.text)
+      #certs.private_key.sign(OpenSSL::Digest::SHA1.new, node.canonicalize(Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0))
+      document = signed_info_node.canonicalize(Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0)
+      expect(our_certificate.public_key.verify(OpenSSL::Digest::SHA1.new, signature_value, document)).to be true
+
     end
   end
 end
