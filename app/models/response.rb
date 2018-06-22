903905e5611d8f8fefa7a56a9a2fa46905c3cb7d
Gary Taylor <gary.taylor@hismessages.com>
2018-06-22 20:56:31 +0100

    Code to import from another s3 bucket is now in a separate service

--- app/models/response.rb
+++ app/models/response.rb
@@ -5,12 +5,7 @@ class Response < ApplicationRecord
   has_many :uploaded_files, through: :response_uploaded_files
 
   def additional_information_key=(value)
-    existing = ai_file
-    if existing
-      update_or_remove_ai_file(value, existing)
-    else
-      create_ai_file(value)
-    end
+    DirectUploadIntoCollectionService.new(collection: uploaded_files, filename: 'additional_information.rtf').import(value)
   end
 
   def has_additional_information_rtf_file?
@@ -19,43 +14,7 @@ class Response < ApplicationRecord
 
   private
 
-  def update_or_remove_ai_file(value, existing)
-    if value.nil?
-      remove_ai_file(existing)
-    else
-      update_ai_file(value, into: existing)
-    end
-  end
-
   def ai_file
     uploaded_files.detect { |file| file.filename == 'additional_information.rtf' }
-  end
-
-  def remove_ai_file(existing)
-    uploaded_files.delete(existing)
-  end
-
-  def create_ai_file(value)
-    return if value.nil?
-    s3_client = ActiveStorage::Blob.service.client.client
-    source_object = s3_client.get_object(bucket: direct_upload_bucket, key: value)
-    blob = ActiveStorage::Blob.create!(filename: 'additional_information.rtf',
-                                       byte_size: source_object.content_length,
-                                       checksum: 'doesntseemtomatter',
-                                       content_type: 'application/rtf',
-                                       metadata: {})
-    object = blob.service.bucket.object(blob.key)
-    object.copy_from(bucket: direct_upload_bucket, key: value)
-    uploaded_files.build(filename: 'additional_information.rtf', file: blob)
-  end
-
-  def update_ai_file(value, into:)
-    blob = into.file.blob
-    object = blob.service.bucket.object(blob.key)
-    object.copy_from(bucket: direct_upload_bucket, key: value)
-  end
-
-  def direct_upload_bucket
-    @direct_upload_bucket ||= Rails.configuration.s3_direct_upload_bucket
   end
 end
