c2914da9c24c0eb5568befdbbc37db4c97a3564b
Gary Taylor <gary.taylor@hismessages.com>
2018-06-27 06:36:27 +0100

    Fixed code climate issues - refactored the event service slightly

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,7 +3,7 @@ class Response < ApplicationRecord
   belongs_to :representative, optional: true
   has_many :response_uploaded_files, dependent: :destroy
   has_many :uploaded_files, through: :response_uploaded_files
-  has_many :pre_allocated_file_keys, as: :allocated_to, dependent: :destroy
+  has_many :pre_allocated_file_keys, as: :allocated_to, dependent: :destroy, inverse_of: :allocated_to
 
   def office
     @office ||= Office.find_by(code: reference[0..1])
--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -41,14 +41,18 @@ class EventService
 
   def handler_proc_for(async, handler, in_process)
     lambda do |*args|
-      if !async && in_process
-        handler_for(handler).handle(*args) unless ignore_events
-      elsif async && !in_process
-        EventWorker.perform_async(handler, *args) unless ignore_events
-      else
-        raise 'Events can only handle sync in process or async out of process right now'
-      end
+      next handle_sync(handler, args) if !async && in_process
+      next handle_with_worker(args, handler) if async && !in_process
+      raise 'Events can only handle sync in process or async out of process right now'
     end
+  end
+
+  def handle_sync(handler, args)
+    handler_instance_for(handler).handle(*args) unless ignore_events
+  end
+
+  def handle_with_worker(args, handler)
+    EventWorker.perform_async(handler, *args) unless ignore_events
   end
 
   def initialize(*)
@@ -63,7 +67,7 @@ class EventService
     handler_procs[event][handler] << handler_proc
   end
 
-  def handler_for(klass)
+  def handler_instance_for(klass)
     handlers[klass] ||= klass.new
   end
 
