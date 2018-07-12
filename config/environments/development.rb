78cc7019ba29ebc55490681cca7f1c41f0952a01
Gary Taylor <gary.taylor@hismessages.com>
2018-07-12 17:18:26 +0100

    Removing double backslashes coming from environment variables for rsa certificates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,19 +55,19 @@ Rails.application.configure do
   # These settings for acas api will be modified once we know exactly what we are doing with them.
   # for now, we are using the test environment settings otherwise the app wont boot.
   if ENV.key?('RSA_ACAS_PUBLIC_CERTIFICATE_BASE64')
-    config.et_acas_api.acas_rsa_certificate = Base64.decode64(ENV['RSA_ACAS_PUBLIC_CERTIFICATE_BASE64'])
+    config.et_acas_api.acas_rsa_certificate = Base64.decode64(ENV['RSA_ACAS_PUBLIC_CERTIFICATE_BASE64'].gsub(/\\n/, "\n"))
   else
     config.et_acas_api.acas_rsa_certificate = File.read(ENV.fetch('RSA_ACAS_PUBLIC_CERTIFICATE', File.absolute_path(Rails.root.join('vendor', 'gems', 'et_acas_api', 'spec', 'acas_interface_support', 'x509', 'theirs', 'publickey.cer'), __dir__)))
   end
 
   if ENV.key?('RSA_ET_PUBLIC_CERTIFICATE_BASE64')
-    config.et_acas_api.rsa_certificate = Base64.decode64(ENV.fetch('RSA_ET_PUBLIC_CERTIFICATE_BASE64'))
+    config.et_acas_api.rsa_certificate = Base64.decode64(ENV.fetch('RSA_ET_PUBLIC_CERTIFICATE_BASE64').gsub(/\\n/, "\n"))
   else
     config.et_acas_api.rsa_certificate = File.read(ENV.fetch('RSA_ET_PUBLIC_CERTIFICATE', File.absolute_path(Rails.root.join('vendor', 'gems', 'et_acas_api', 'spec', 'acas_interface_support', 'x509', 'ours', 'publickey.cer'), __dir__)))
   end
 
   if ENV.key?('RSA_ET_PRIVATE_KEY_BASE64')
-    config.et_acas_api.rsa_private_key = Base64.decode64(ENV.fetch('RSA_ET_PRIVATE_KEY_BASE64'))
+    config.et_acas_api.rsa_private_key = Base64.decode64(ENV.fetch('RSA_ET_PRIVATE_KEY_BASE64').gsub(/\\n/, "\n"))
   else
     config.et_acas_api.rsa_private_key = File.read(ENV.fetch('RSA_ET_PRIVATE_KEY', File.absolute_path(Rails.root.join('vendor', 'gems', 'et_acas_api', 'spec', 'acas_interface_support', 'x509', 'ours', 'privatekey.pem'), __dir__)))
   end
