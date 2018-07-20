b97afa7fd89f8acb322ae97a1ca813dccb7646c3
Gary Taylor <gary.taylor@hismessages.com>
2018-07-20 14:04:48 +0100

    Data migrations for roles and permissions

--- db/migrate/20180719065824_add_username_to_admin_users.rb
+++ db/migrate/20180719065824_add_username_to_admin_users.rb
@@ -3,5 +3,6 @@ class AddUsernameToAdminUsers < ActiveRecord::Migration[5.2]
     add_column :admin_users, :username, :string
     add_column :admin_users, :name, :string
     add_column :admin_users, :department, :string
+    add_index :admin_users, :username, unique: true
   end
 end
