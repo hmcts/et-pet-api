8444903475e9778c4069bb22a3fe56794a9a4299
Sivvoy Taing <sivvo@yahoo.com>
2018-07-23 14:28:31 +0100

    replace etsystem@justice.gsi.gov.uk with no-reply@digital.justice.gov.uk:

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_options = { from: 'etsystem@justice.gsi.gov.uk' }
+  config.action_mailer.default_options = { from: 'no-reply@digital.justice.gov.uk' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
