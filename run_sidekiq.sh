e0267a228ffec8ce4cac473f337a97887d2bc0b1
Isher Singh Raikmo <35302598+isherraikmo@users.noreply.github.com>
2018-08-07 13:37:43 +0100

    further papertrail changes

--- run_sidekiq.sh
+++ run_sidekiq.sh
@@ -2,6 +2,6 @@
 
 echo "Running remote-syslog"
 
-remote_syslog -p 20568 -d logs7.papertrailapp.com --pid-file=/var/run/remote_syslog.pid --hostname=$PAPERTRAIL_NAME-$HOSTNAME /usr/src/app/log/production.log
+remote_syslog -p 20568 -d logs7.papertrailapp.com --pid-file=/var/run/remote_syslog.pid --hostname=$PAPERTRAIL_NAME /usr/src/app/log/production.log
 
 bundle exec sidekiq -C config/sidekiq.yml
