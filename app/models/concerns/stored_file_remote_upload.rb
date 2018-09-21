3f1ad3a15a8c9fd06597b1e98c1879d118e99484
Gary Taylor <gary.taylor@hismessages.com>
2018-09-21 15:12:32 +0100

    Add functionality to uploaded_file model to be able to import from remote url

---
+++ app/models/concerns/stored_file_remote_upload.rb
@@ -0,0 +1,23 @@
+require 'net/http'
+require 'tempfile'
+require 'uri'
+# This module is included in any models that need to be able to upload a file from a remote url
+#
+module StoredFileRemoteUpload
+  extend ActiveSupport::Concern
+
+  # Imports a remote file
+  #
+  # @param [String] url The url of the file to import
+  def import_file_url=(url)
+    file = Tempfile.new
+    file.binmode
+    response = HTTParty.get(url, stream_body: true) do |chunk|
+      file.write chunk
+    end
+    file.flush
+    self.file = ActionDispatch::Http::UploadedFile.new filename: filename || File.basename(url),
+                                           tempfile: file,
+                                           type: response.content_type
+  end
+end
