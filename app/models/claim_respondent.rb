01132e7f6ae11c2975ab46b5c5b7741578ce95a8
Gary Taylor <gary.taylor@hismessages.com>
2018-10-04 17:49:24 +0100

    Claim respondent model now has default sort in attempt to fix random issues where the order in the association comes out different to the order inserted

--- app/models/claim_respondent.rb
+++ app/models/claim_respondent.rb
@@ -5,5 +5,5 @@
 class ClaimRespondent < ApplicationRecord
   belongs_to :claim
   belongs_to :respondent
-  default_scope { order(created_at: :desc) }
+  default_scope { order(created_at: :asc) }
 end
