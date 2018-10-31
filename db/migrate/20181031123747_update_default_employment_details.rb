8f8190109acdf3441820eb11f87dff2343c2f303
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-10-31 15:03:44 +0000

    Declare the Class and table_name before changing default value

--- db/migrate/20181031123747_update_default_employment_details.rb
+++ db/migrate/20181031123747_update_default_employment_details.rb
@@ -1,4 +1,8 @@
 class UpdateDefaultEmploymentDetails < ActiveRecord::Migration[5.2]
+  class Claim < ActiveRecord::Base
+    self.table_name = :claims
+  end
+
   def up
     Claim.where(employment_details: "{}").update_all(employment_details: {})
   end
