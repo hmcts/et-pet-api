b0af739f33a88720c113860c1412d1880417644e
Gary Taylor <gary.taylor@hismessages.com>
2018-11-27 11:00:58 +0000

    Claim handler now split up and the atos stuff is now in the gem and non atos stuff in the app (apart from pdf which will get moved back)

--- app/event_handlers/claim_import_multiple_claimants_handler.rb
+++ app/event_handlers/claim_import_multiple_claimants_handler.rb
@@ -6,20 +6,4 @@ class ClaimImportMultipleClaimantsHandler
     claim.save!
     EventService.publish('ClaimMultipleClaimantsImported', claim)
   end
-
-  private
-
-  def rename_csv_file(claim:)
-    file = claim.claimants_csv_file
-    return if file.nil?
-    claimant = claim.primary_claimant
-    file.filename = "et1a_#{claimant[:first_name].tr(' ', '_')}_#{claimant[:last_name]}.csv"
-  end
-
-  def rename_rtf_file(claim:)
-    file = claim.uploaded_files.detect { |f| f.filename.ends_with?('.rtf') }
-    return if file.nil?
-    claimant = claim.primary_claimant
-    file.filename = "et1_attachment_#{claimant[:first_name].tr(' ', '_')}_#{claimant[:last_name]}.rtf"
-  end
 end
