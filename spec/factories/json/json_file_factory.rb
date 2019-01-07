cf07033d9210ab8ec6124c2e9a5bc1aa8ee29e55
Gary Taylor <gary.taylor@hismessages.com>
2019-01-07 15:25:34 +0000

    Updated factories to use blocks for static values to prevent warnings - no impact on implementation code

--- spec/factories/json/json_file_factory.rb
+++ spec/factories/json/json_file_factory.rb
@@ -3,11 +3,11 @@ require 'faker'
 FactoryBot.define do
 
   factory :json_file_data, class: ::EtApi::Test::Json::Node do
-    data_url nil
+    data_url { nil }
     trait :et1_first_last_pdf do
-      filename 'et1_first_last.pdf'
-      checksum 'ee2714b8b731a8c1e95dffaa33f89728'
-      data_from_key nil
+      filename { 'et1_first_last.pdf' }
+      checksum { 'ee2714b8b731a8c1e95dffaa33f89728' }
+      data_from_key { nil }
       after(:build) do |obj, _e|
         uploaded_file = create(:uploaded_file, :example_pdf)
         obj.data_url = uploaded_file.file.blob.service_url
@@ -15,9 +15,9 @@ FactoryBot.define do
     end
 
     trait :simple_user_with_csv_group_claims do
-      filename 'simple_user_with_csv_group_claims.csv'
-      checksum '7ac66d9f4af3b498e4cf7b9430974618'
-      data_from_key nil
+      filename { 'simple_user_with_csv_group_claims.csv' }
+      checksum { '7ac66d9f4af3b498e4cf7b9430974618' }
+      data_from_key { nil }
       after(:build) do |obj, _e|
         uploaded_file = create(:uploaded_file, :example_claim_claimants_csv)
         obj.data_url = uploaded_file.file.blob.service_url
@@ -25,9 +25,9 @@ FactoryBot.define do
     end
 
     trait :simple_user_with_rtf do
-      filename 'simple_user_with_rtf.rtf'
-      checksum 'e69a0344620b5040b7d0d1595b9c7726'
-      data_from_key nil
+      filename { 'simple_user_with_rtf.rtf' }
+      checksum { 'e69a0344620b5040b7d0d1595b9c7726' }
+      data_from_key { nil }
       after(:build) do |obj, _e|
         uploaded_file = create(:uploaded_file, :example_claim_rtf)
         obj.data_url = uploaded_file.file.blob.service_url
--- spec/factories/json/json_representative_factory.rb
+++ spec/factories/json/json_representative_factory.rb
@@ -14,17 +14,17 @@ FactoryBot.define do
 
     trait :private_individual do
       minimal
-      name 'Jane Doe'
-      organisation_name 'repco ltd'
+      name { 'Jane Doe' }
+      organisation_name { 'repco ltd' }
       association :address_attributes, :rep_address, factory: :json_address_data
-      address_telephone_number '0207 987 6543'
-      mobile_number '07987654321'
-      representative_type 'Private Individual'
-      dx_number 'dx address'
-      reference 'Rep Ref'
-      contact_preference 'fax'
-      email_address 'test@email.com'
-      fax_number '0207 345 6789'
+      address_telephone_number { '0207 987 6543' }
+      mobile_number { '07987654321' }
+      representative_type { 'Private Individual' }
+      dx_number { 'dx address' }
+      reference { 'Rep Ref' }
+      contact_preference { 'fax' }
+      email_address { 'test@email.com' }
+      fax_number { '0207 345 6789' }
     end
   end
 end
