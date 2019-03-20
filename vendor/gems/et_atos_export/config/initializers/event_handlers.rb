f5da6b2b049500b47f8049a024c48a561b799d77
Gary Taylor <gary.taylor@hismessages.com>
2019-03-20 09:20:49 +0000

    Bug fix - duplicated exports

--- vendor/gems/et_atos_export/config/initializers/event_handlers.rb
+++ vendor/gems/et_atos_export/config/initializers/event_handlers.rb
@@ -1,5 +1,4 @@
 Rails.application.config.after_initialize do |app|
   app.event_service.subscribe('ClaimPrepared', EtAtosExport::ExportClaimHandler, async: true, in_process: false)
   app.event_service.subscribe('ResponsePrepared', EtAtosExport::ExportResponseHandler, async: true, in_process: false)
-  app.event_service.subscribe('ClaimPdfFileAdded', EtAtosExport::ExportClaimHandler, async: false, in_process: true)
 end
