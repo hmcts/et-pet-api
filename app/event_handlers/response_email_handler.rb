98cdd98cef5a7422eadd16a38360a85d2264ac68
Gary Taylor <gary.taylor@hismessages.com>
2019-03-21 14:12:49 +0000

    Response email handler now gets delivered inline with the rest of the prepare response handler

--- app/event_handlers/response_email_handler.rb
+++ app/event_handlers/response_email_handler.rb
@@ -7,6 +7,6 @@ class ResponseEmailHandler
 
   def send_email(response, template_reference: response.email_template_reference)
     office = OfficeService.lookup_by_case_number(response.case_number)
-    ResponseMailer.with(response: response, office: office, template_reference: template_reference).confirmation_email.deliver_later
+    ResponseMailer.with(response: response, office: office, template_reference: template_reference).confirmation_email.deliver_now
   end
 end
