894fb35c14a03f6d75ec088c8d1d4bbe47843d71
Gary Taylor <gary.taylor@hismessages.com>
2019-04-10 15:41:44 +0100

    Sidekiq Log level is configurable via environment variable

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -23,3 +23,5 @@ Sidekiq.configure_client do |config|
   redis_config[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD'].present?
   config.redis = redis_config
 end
+
+Sidekiq::Logging.logger.level = ::Logger.const_get(ENV.fetch('RAILS_LOG_LEVEL', 'debug').upcase)
