29b4d7f291fecf9126243fb36f584d9c4aa76b52
Gary Taylor <gary.taylor@hismessages.com>
2019-08-29 14:18:40 +0100

    Added ResponseQueuedForExport event

--- app/event_handlers/response_export_handler.rb
+++ app/event_handlers/response_export_handler.rb
@@ -1,7 +1,9 @@
 class ResponseExportHandler
   def handle(response)
     ExternalSystem.containing_office_code(response.office_code).each do |system|
-      Export.responses.create resource: response, external_system_id: system.id
+      export = Export.responses.create resource: response, external_system_id: system.id
+      Rails.application.event_service.publish('ResponseQueuedForExport', export) if system.enabled? && system.export?
+
     end
 
     response.save
