ef8cc168335dc750d3ff1cc509309e69c11e3480
Gary Taylor <gary.taylor@hismessages.com>
2019-09-24 15:45:04 +0100

    Using exporter gem for this branch

--- app/event_handlers/response_export_handler.rb
+++ app/event_handlers/response_export_handler.rb
@@ -2,7 +2,7 @@ class ResponseExportHandler
   def handle(response)
     ExternalSystem.containing_office_code(response.office_code).where(enabled: true, export_responses: true).each do |system|
       export = Export.responses.create resource: response, external_system_id: system.id
-      Rails.application.event_service.publish('ResponseQueuedForExport', export)
+      Rails.application.event_service.publish('ResponseQueuedForExport', export) unless system.export_queue.blank?
     end
 
     response.save
