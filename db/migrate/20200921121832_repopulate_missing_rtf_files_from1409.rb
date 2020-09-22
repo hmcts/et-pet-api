8ee3327d518877bca296690323e9511340c81104
Gary Taylor <gary.taylor@hismessages.com>
2020-09-22 07:37:03 +0100

    RST-2865 Since the data outage on 14/9 claim additional info file is missing

--- db/migrate/20200921121832_repopulate_missing_rtf_files_from1409.rb
+++ db/migrate/20200921121832_repopulate_missing_rtf_files_from1409.rb
@@ -16,10 +16,12 @@ class RepopulateMissingRtfFilesFrom1409 < ActiveRecord::Migration[6.0]
       claim = Claim.where(reference: reference).first
       next if claim.nil? || claim.uploaded_files.where('filename LIKE ?', '%.rtf').count > 0
 
+      announce "Adding new RTF file for claim id #{claim.id} reference #{claim.reference}"
       input_data = file_command['data']
       uploaded_file = claim.uploaded_files.create(input_data.merge(import_file_url: input_data.delete('data_url'), import_from_key: input_data.delete('data_from_key')))
       UploadedFileImportService.import_file_url(uploaded_file.import_file_url, into: uploaded_file)
       claim.save(touch:false)
+      announce "Added new RTF file for claim id #{claim.id} reference #{claim.reference}"
     end
   end
 
