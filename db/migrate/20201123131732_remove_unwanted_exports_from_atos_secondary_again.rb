6f8d1bb65cae9fa47e9e486c1df33ec7b1b928dc
Gary Taylor <gary.taylor@hismessages.com>
2020-11-23 13:21:50 +0000

    RST-3010 Added migration to tidy up exports that are no longer needed (running without the state checking)

---
+++ db/migrate/20201123131732_remove_unwanted_exports_from_atos_secondary_again.rb
@@ -0,0 +1,16 @@
+class RemoveUnwantedExportsFromAtosSecondaryAgain < ActiveRecord::Migration[6.0]
+  def up
+    execute <<-SQL
+      DELETE FROM exports WHERE exports.id IN (
+        SELECT DISTINCT exports.id AS id FROM exports
+        JOIN events ON events.attached_to_id = exports.resource_id AND events.attached_to_type = exports.resource_type AND events.name = 'ClaimExportedToAtosQueue'
+        JOIN external_systems ON exports.external_system_id = external_systems.id
+        WHERE external_systems.reference = 'atos_secondary'
+      );
+    SQL
+  end
+
+  def down
+    # We cannot undo this - but we don't want anything to fail
+  end
+end
