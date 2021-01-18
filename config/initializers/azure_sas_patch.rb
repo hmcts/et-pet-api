125a7bf57c3c7841e4e832b1d7765a9bb80af19b
Gary Taylor <gary.taylor@hismessages.com>
2021-01-18 11:14:49 +0000

    RST-3052 Patched azure signature generator which didnt work with azurite

--- config/initializers/azure_sas_patch.rb
+++ config/initializers/azure_sas_patch.rb
@@ -1,17 +1,18 @@
 require 'azure/storage/common/core/auth/shared_access_signature_generator'
 # We all hate patches - I know !!
 #
-# But, at the moment, active storage can only work with azure-storage gem 0.15.0.preview (it has been in this state for over a year !!)
-# I have put in a PR for this change into azure-storage-ruby gem - but we will see if it gets merged.  In the mean time, we must put
-# up with this patch to allow us to parse xml data from both azure and azurite (test environments).
-#
-# The only change is the addition of ', nil, nil, Nokogiri::XML::ParseOptions::DEFAULT_XML | Nokogiri::XML::ParseOptions::NOBLANKS' to the Nokogiri.Slop call
-# which does at it says on the tin - if we have blank nodes containing just whitespace (\n was the culprit)
+# But, at the moment, active storage will not generate valid SAS signatures when connected to azurite simulator.
+# The bug occurs as the path contains the account name which is valid when using the simulator (as you cannot
+# use subdomains like normal azure blob storage) - however, the signature generator adds the account name
+# to the body of the text that it generates the signature for.
 #
 # So, to prevent you going mad when you upgrade for a different reason, this patch will deliberately fail, forcing you to come back to this file
-# and read this comment - then deciding if the patch is still needed.
+# and read this comment - then deciding if the patch is still needed.  If it is, simply update the version checker in the code below
 #
 
+if Azure::Storage::Common::Version.to_s != '2.0.2'
+  raise "The patch in #{__FILE__} is only compatible with azure-storage-common version 2.0.2 - please check if this is still required by running tests without it"
+end
 module Azure
   module Storage
     module Common
@@ -20,6 +21,7 @@ module Azure
           class SharedAccessSignature
             private
 
+            # This patched version of canonicalized_resource will remove duplicate account names in the path
             def canonicalized_resource(service_type, path)
               optional_account_name = if path.start_with?("/#{account_name}/")
                                         ''
