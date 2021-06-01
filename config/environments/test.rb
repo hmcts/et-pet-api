e76e1251c0b468f64413a57d4502bb9dc881ed50
Gary Taylor <gary.taylor@hismessages.com>
2021-06-01 10:06:52 +0100

    HOTFIX Changed from address in feedback mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.active_storage.service_urls_expire_in = 10.days
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_options = { from: 'no-reply@digital.justice.gov.uk' }
+  config.action_mailer.default_options = { from: 'no-reply@localhost' }
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
