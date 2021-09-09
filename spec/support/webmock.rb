1c36d617898a781e6ce63dc512c29a7912007ba1
garytaylor <gary.taylor@hismessages.com>
2021-09-09 07:30:31 +0000

    RST-3662 Title field can now be null in claimant

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,6 +1,6 @@
 require 'webmock/rspec'
 other_hosts = [
   'http://azurite:10000',
-  'http://azure_blob_storage.et.127.0.0.1.nip.io:3100'
+  ENV.fetch('AZURE_STORAGE_BLOB_HOST', 'http://azure_blob_storage.et.127.0.0.1.nip.io:3100')
 ]
 WebMock.disable_net_connect!(allow_localhost: true, allow: other_hosts)
