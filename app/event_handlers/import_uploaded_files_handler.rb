a9a6d915e1781e3a94bea7f60e94c6a20463deac
garytaylor <gary.taylor@hismessages.com>
2022-03-21 13:02:36 +0000

    RST-4340 Fixed validation issue for csv file and titleized 'title' column (#587)

--- app/event_handlers/import_uploaded_files_handler.rb
+++ app/event_handlers/import_uploaded_files_handler.rb
@@ -1,7 +1,7 @@
 class ImportUploadedFilesHandler
   def handle(root_object)
     root_object.uploaded_files.includes(:file_attachment).each do |uploaded_file|
-      next if uploaded_file.import_from_key.nil? && uploaded_file.import_file_url.nil?
+      next if uploaded_file.file.attached? || (uploaded_file.import_from_key.nil? && uploaded_file.import_file_url.nil?)
 
       UploadedFileImportService.import_from_key(uploaded_file.import_from_key, into: uploaded_file)
       UploadedFileImportService.import_file_url(uploaded_file.import_file_url, into: uploaded_file)
