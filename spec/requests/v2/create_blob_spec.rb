f439d3082baaa188b5e0141fd1967f2566c83d10
garytaylor <gary.taylor@hismessages.com>
2023-01-11 10:42:32 +0000

    (RST-5002) Added application/csv mime type to registry (#614)

--- spec/requests/v2/create_blob_spec.rb
+++ spec/requests/v2/create_blob_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'Create a blob in the direct upload storage', type: :request do
   describe 'POST /api/v2/create_blob' do
     it 'stores the csv file as a blob in the direct upload storage and identifies the content type as csv' do
       post '/api/v2/create_blob', params: { file: Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/simple_user_with_csv_group_claims.csv'), 'application/csv', false) }, headers: default_headers
-      expect(json_response).to include data: a_hash_including(key: instance_of(String), content_type: 'text/csv', filename: 'simple_user_with_csv_group_claims.csv')
+      expect(json_response).to include data: a_hash_including(key: instance_of(String), content_type: 'application/csv', filename: 'simple_user_with_csv_group_claims.csv')
     end
 
     it 'stores the rtf file as a blob in the direct upload storage and identifies the content type as rtf' do
