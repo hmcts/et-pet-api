139cd24f3fd5a5e5475720ad3214750a18208d56
Gary Taylor <gary.taylor@hismessages.com>
2026-07-23 22:49:42 +0100

    (RST-8372) Database setup on startup

--- run_solid_queue.sh
+++ run_solid_queue.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+case ${DOCKER_STATE} in
+migrate)
+    echo "Preparing databases"
+    bundle exec rails db:prepare
+    ;;
+create)
+    echo "Running create"
+    bundle exec rake db:create
+    bundle exec rake db:migrate
+    bundle exec rake db:seed
+    ;;
+esac
+
 exec bin/rails server --binding 0.0.0.0 --port "${PORT:-8080}"
