2fd85414c7bac55a6ab364cfdd3a3557ea2091d1
Gary Taylor <gary.taylor@hismessages.com>
2026-07-24 07:57:32 +0100

    (RST-8372) Removed extension from queue schema - as it is on by default anyway

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2025_10_31_072154) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "active_admin_comments", force: :cascade do |t|
     t.bigint "author_id"
     t.string "author_type"
