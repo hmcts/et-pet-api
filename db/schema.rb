a7eded83938d5c8f346a94e621c8f9be560bec84
Gary Taylor <gary.taylor@hismessages.com>
2026-09-07 09:25:34 +0100

    (rst-8542) Fixed schema and updated pdf

--- db/schema.rb
+++ db/schema.rb
@@ -178,6 +178,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_18_074143) do
 
   create_table "claims", force: :cascade do |t|
     t.boolean "administrator"
+    t.string "case_heard_by_preference"
+    t.string "case_heard_by_preference_reason"
     t.string "case_type"
     t.text "claim_details"
     t.integer "claimant_count", default: 0, null: false
@@ -454,6 +456,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_18_074143) do
     t.boolean "agree_with_early_conciliation_details"
     t.string "agree_with_earnings_details"
     t.string "agree_with_employment_dates"
+    t.string "case_heard_by_preference"
+    t.string "case_heard_by_preference_reason"
     t.string "case_number"
     t.string "claim_information"
     t.string "claimants_name"
